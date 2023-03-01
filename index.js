dcee7095261b43d27dbf68684d64e8fd53e76840
Slava Ivanov <drumtrack@ya.ru>
2023-03-01 15:01:59 +0300

    feat: public path

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use((req, res, next) => {
+
+});
+app.listen(PORT, () => console.log(`start server at port ${PORT}`));
